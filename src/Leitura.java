d67f062c8a07b1364ce8d83eba88980b65637588
Gustavo Emiliano <gustavoemiliano2014@gmail.com>
2024-10-25 16:24:04 -0300

    add: utilização da classe Scanner e praticando leitura de dados

---
+++ src/Leitura.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Leitura {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Digite seu filme favorito: ");
+        String filme = leitura.nextLine();
+
+        System.out.println("Entre com o ano de lançamento: ");
+        int anoLancamento = leitura.nextInt();
+
+        System.out.println("Entre com a sua avaliação para o filme: ");
+        double avaliacao = leitura.nextDouble();
+
+        System.out.println(filme);
+        System.out.println(anoLancamento);
+        System.out.println(avaliacao);
+    }
+}
